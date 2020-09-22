2929384ab670607699218275f1d15bb2bdbce04a
Rico Stuivenberg <rico.stuivenberg@student.hu.nl>
2020-09-22 14:53:41 +0200

    :lipstick: alsjeblieft raymond

--- src/main/java/hu/adsd/ClimateApp.java
+++ src/main/java/hu/adsd/ClimateApp.java
@@ -4,23 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.util.List;
-import java.util.Scanner;
 
 public class ClimateApp extends Application
 {
     // Class variables
-    //
     private final Project project;
     private final List<Material> availableMaterials;
-    //
-    private double xOffset;
-    private double yOffset;
 
     // Constructor
     public ClimateApp()
@@ -36,21 +28,9 @@ public class ClimateApp extends Application
     {
         Parent root = FXMLLoader.load( getClass().getResource( "../../sample.fxml" ) );
         Scene scene = new Scene( root );
-        scene.setFill( Color.TRANSPARENT);
 
         stage.setScene(scene);
-        stage.initStyle( StageStyle.TRANSPARENT);
         stage.setTitle("Klimaat App");
         stage.show();
-
-        root.setOnMousePressed( ( MouseEvent event) -> {
-            xOffset = event.getSceneX();
-            yOffset = event.getSceneY();
-        });
-
-        root.setOnMouseDragged((javafx.scene.input.MouseEvent event) -> {
-            stage.setX(event.getScreenX() - xOffset);
-            stage.setY(event.getScreenY() - yOffset);
-        });
     }
 }
--- src/main/java/hu/adsd/Controller.java
+++ src/main/java/hu/adsd/Controller.java
@@ -1,12 +1,20 @@
 package hu.adsd;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 
-public class Controller
+public class Controller implements Initializable
 {
     @FXML
     private ImageView OffImage2, TableViewAwayGlassImage;
@@ -15,25 +23,28 @@ public class Controller
     private Button AddButton, TableViewButton, TableViewLargeAwayButton;
 
     @FXML
-    private TableView TableViewLarge;
+    private TableView<Material> materialTable;
 
+    @FXML
+    private TableColumn<Material, String> productColumn;
 
-    public void closeApp( ActionEvent event )
-    {
-        System.exit( 0 );
-    }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        
+        // stap 1 arraylist maken
+        ArrayList<Material> = new ArrayList<Material>();
 
-    public void OpenTableViewLarge( ActionEvent event )
-    {
-        TableViewLarge.setVisible( true );
-        TableViewLargeAwayButton.setVisible( true );
-        TableViewAwayGlassImage.setVisible( true );
-    }
+        // stap 2 observable arraylist maken (javafx functie)
+        // ObservableList materialData
 
-    public void CloseTableViewLarge( ActionEvent event )
-    {
-        TableViewLarge.setVisible( false );
-        TableViewLargeAwayButton.setVisible( false );
-        TableViewAwayGlassImage.setVisible( false );
-    }
+        // stap 3 cell value properties
+        // productColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        
+        // stap 4 set items van de tabel
+        // materialTable.setItems(materialData);
+	}
+
+    
+
 }
