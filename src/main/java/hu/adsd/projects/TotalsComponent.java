1aa91e57b38ac81f176a13453a68c4c35bc66560
Raymond Schonewille <schonewillerf@gmail.com>
2020-10-14 14:22:11 +0200

    :lipstick: prepared project for styling

--- src/main/java/hu/adsd/projects/TotalsComponent.java
+++ src/main/java/hu/adsd/projects/TotalsComponent.java
@@ -1,27 +1,28 @@
 package hu.adsd.projects;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-public class TotalsComponent extends VBox implements Initializable {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TotalsComponent extends VBox implements Initializable
+{
 
     private String title;
 
     @FXML
     private Label circulationTypeLabel;
 
-    public TotalsComponent(String title)
+    public TotalsComponent( String title )
     {
         this.title = title;
 
-        FXMLLoader fxmlLoader = new FXMLLoader( getClass().getResource( "../../../projectBuildingMaterialConfigCardView.fxml" ) );
+        FXMLLoader fxmlLoader = new FXMLLoader( getClass().getResource( "../../../projectConfigTotal.fxml" ) );
         fxmlLoader.setRoot( this );
         fxmlLoader.setController( this );
 
@@ -36,16 +37,8 @@ public class TotalsComponent extends VBox implements Initializable {
     }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-        // commented because of dropdown in the same fxml, raymond will make seperate fxml later
-        // circulationTypeLabel.setText("Totaal " + title + ":");
-
-    }
-
-    public void buttonTest()
+    public void initialize( URL location, ResourceBundle resources )
     {
-        System.out.println("test van knop");
+
     }
-    
 }
