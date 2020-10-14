f22278dccf83d90616c5b1e7952bc5b84e7ff647
Raymond Schonewille <schonewillerf@gmail.com>
2020-10-14 11:18:15 +0200

    :zap: :sparkles: Added Quantity Edit and Save

--- src/main/java/hu/adsd/projects/projectBuildingMaterialCard.java
+++ src/main/java/hu/adsd/projects/projectBuildingMaterialCard.java
@@ -4,10 +4,15 @@ import hu.adsd.buildingmaterials.Product;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,11 +20,25 @@ public class projectBuildingMaterialCard extends VBox implements Initializable
 {
     private final Product product;
 
+    private boolean editQuantity;
+
     // Labels buttons etc for BuildingMaterialCard
     @FXML
     private Label nameLabel;
 
-    public projectBuildingMaterialCard( Product product)
+    @FXML
+    private ImageView productImage;
+
+    @FXML
+    private TextField quantityField;
+
+    @FXML
+    private Button editOrSaveButton;
+
+    @FXML
+    private Button cancelButton;
+
+    public projectBuildingMaterialCard( Product product )
     {
         // set the product
         this.product = product;
@@ -45,5 +64,51 @@ public class projectBuildingMaterialCard extends VBox implements Initializable
     {
         // Set labels etc
         nameLabel.setText( this.product.getName() );
+
+        InputStream inputStream = getClass().getResourceAsStream( this.product.getImagePath() );
+        Image image = new Image( inputStream );
+        productImage.setImage( image );
+
+        quantityField.setText( Integer.toString( this.product.getQuantity() ) );
+
+        editOrSaveButton.setOnAction( event -> editOrSaveButtonClicked() );
+        cancelButton.setOnAction( event -> cancelButtonClicked() );
+    }
+
+    void editOrSaveButtonClicked()
+    {
+        if ( !editQuantity )
+        {
+            System.out.println( "make editable" );
+
+            quantityField.setEditable( true );
+            editOrSaveButton.setText( "Opslaan" );
+            cancelButton.setDisable( false );
+
+            editQuantity = true;
+        }
+        else
+        {
+            System.out.println("save quantity");
+
+            quantityField.setEditable( false );
+            editOrSaveButton.setText( "Aanpassen" );
+            cancelButton.setDisable( true );
+
+            this.product.setQuantity( Integer.parseInt( quantityField.getText() ) );
+
+            editQuantity = false;
+        }
+    }
+
+    void cancelButtonClicked()
+    {
+        System.out.println("cancel edit");
+
+        quantityField.setEditable( false );
+        quantityField.setText( Integer.toString( this.product.getQuantity() ) );
+
+        editOrSaveButton.setText( "Aanpassen" );
+        cancelButton.setDisable( true );
     }
 }
