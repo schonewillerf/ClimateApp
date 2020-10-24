3e372b6abc34556c7e7f0c76fc2f0871ee3f2600
Jan Ayuda <jan.ayuda@student.hu.nl>
2020-10-24 12:27:42 +0200

    :alien: API working

--- src/main/java/hu/adsd/dataservice/DatabaseHandler.java
+++ src/main/java/hu/adsd/dataservice/DatabaseHandler.java
@@ -3,8 +3,15 @@ package hu.adsd.dataservice;
 import hu.adsd.products.CirculationType;
 import hu.adsd.products.Product;
 import hu.adsd.products.ProductSort;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.sqlite.SQLiteDataSource;
 
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -63,6 +70,44 @@ class DatabaseHandler
             throwables.printStackTrace();
         }
 
+        return productsList;
+    }
+    
+    public List<Product> getProductsFilteredSortedAndByRoomFromAPI( String filter, ProductSort productsort, String room )
+    {
+        List<Product> productsList = new ArrayList<>();
+
+        JSONParser parser = new JSONParser();
+
+        try {
+            URL url = new URL( "http://localhost:8080/data" );
+            URLConnection urlConnection = url.openConnection();
+            JSONArray array = (JSONArray) parser.parse( new InputStreamReader(urlConnection.getInputStream()) );
+
+            for ( Object object : array )
+            {
+                JSONObject producObject = (JSONObject) object;
+
+                Long id = (Long) producObject.get( "id" );
+                String name = (String) producObject.get( "name" );
+                double carbon = (Double) producObject.get( "carbon" );
+                double energy = (Double) producObject.get( "energy" );
+                double refCarbon = (Double) producObject.get( "refCarbon" );
+                double refEnergy = (Double) producObject.get( "refEnergy" );
+                String roomAPI = (String) producObject.get( "room" );
+
+                Product product = new Product( id.intValue(), name, 100, CirculationType.LINEAR, carbon, energy, refCarbon, refEnergy, roomAPI );
+
+                if ( product.getBuildingPart().equals(room) )
+                {
+                    productsList.add(product);
+                }
+            }
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
         return productsList;
     }
 }
