a585ed29371dd034d275e42b3f293ac8604edd48
Raymond Schonewille <schonewillerf@gmail.com>
2020-10-26 20:20:38 +0100

    :zap: preparations for Project-Products-Configuration are ready

--- src/main/java/hu/adsd/projects/ProductsConfigurationController.java
+++ src/main/java/hu/adsd/projects/ProductsConfigurationController.java
@@ -1,11 +1,29 @@
 package hu.adsd.projects;
 
 import hu.adsd.ClimateApp;
+import javafx.fxml.Initializable;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ProductsConfigurationController
+public class ProductsConfigurationController implements Initializable
 {
+    ProductsConfiguration productsConfiguration;
+
+    public ProductsConfigurationController( int selectedConfiguration )
+    {
+        this.productsConfiguration = ClimateApp
+                .getProject()
+                .getConfigurations()
+                .get( selectedConfiguration );
+    }
+
+    @Override
+    public void initialize( URL url, ResourceBundle resourceBundle )
+    {
+
+    }
 
     public void goToProject() throws IOException
     {
