983f9b3abda9f31290eea22f2c1decbb10856beb
Raymond Schonewille <schonewillerf@gmail.com>
2020-10-28 10:09:15 +0100

    :tada: improved product add

--- src/main/java/hu/adsd/projects/ProductsConfiguration.java
+++ src/main/java/hu/adsd/projects/ProductsConfiguration.java
@@ -1,6 +1,5 @@
 package hu.adsd.projects;
 
-import hu.adsd.products.CirculationType;
 import hu.adsd.products.Product;
 
 import java.util.ArrayList;
@@ -20,14 +19,6 @@ public class ProductsConfiguration
         this.embodiedEnergy = "0 mg / 0 kJ";
         this.numberOfProducts = 0;
         this.buildingParts = new ArrayList<>();
-
-        // Add some random products for testing the BOM generation
-        // Code is only temporary until adding products works properly again
-        BuildingPart myTest = new BuildingPart( "dak" );
-        myTest.getProducts().add( new Product( 1, "Raymond", 1, CirculationType.LINEAR, 0,0,0,0,"dak" ) );
-        myTest.getProducts().add( new Product( 2, "is", 100000, CirculationType.LINEAR, 0,0,0,0,"dak" ) );
-        myTest.getProducts().add( new Product( 3, "Awesome", 9999999, CirculationType.LINEAR, 0,0,0,0,"dak" ) );
-        this.buildingParts.add( myTest );
     }
 
     public String getName()
@@ -68,5 +59,33 @@ public class ProductsConfiguration
     public void setBuildingParts( List<BuildingPart> buildingParts )
     {
         this.buildingParts = buildingParts;
+    }
+
+    public void addProduct( Product product )
+    {
+        // Increment number of products
+        this.numberOfProducts += product.getQuantity();
+
+        // Loop over existing BuildingParts
+        for ( BuildingPart buildingPart : buildingParts )
+        {
+            // Check if building part matches product building part
+            if ( buildingPart.getName().equals( product.getBuildingPart() ))
+            {
+                // Add product to building part if there is a match
+                buildingPart.getProducts().add( product );
+                // Also exit the method after product was added
+                return;
+            }
+        }
+
+        // Will only execute if product is not added yet
+        //
+        // Add product to new building part
+        BuildingPart buildingPart = new BuildingPart( product.getBuildingPart() );
+        buildingPart.getProducts().add( product );
+        //
+        // Add building part to configuration
+        buildingParts.add( buildingPart );
     }
 }
